6d9dc20ee469f5494b93cd9de8e9f0ece77e00da
mandonando007 <jesusarmando51@gmail.com>
2022-05-12 12:51:44 -0500

    Iconos 2
    12:51pm

--- app/src/main/java/com/example/ensaame/VideosFragment.kt
+++ app/src/main/java/com/example/ensaame/VideosFragment.kt
@@ -1,12 +1,8 @@
 package com.example.ensaame
 
-import android.os.Bundle
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 
-class VideosFragment : Fragment(R.layout.fragment_videos) {
+class VideosFragment : Fragment(R.layout.fragment_glosario) {
 
     }
 
